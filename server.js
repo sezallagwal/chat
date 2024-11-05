ce4ccaa8da22135b54c699cacf99c853e1f61d51
sezallagwal <sezal1470@gmail.com>
2024-11-05 13:42:00 +0530

    another fix

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ io.on("connection", async (socket) => {
   });
 });
 
-server.listen(4000, () => {
-  console.log("Server is running on port 4000");
+server.listen(10000, () => {
+  console.log("Server is running on port 10000");
 });
